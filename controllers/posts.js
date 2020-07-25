96d47010f7dd1c2a8edc38bdc1f091e422ac0c24
bineetNaidu <bineet.s.naidu@gmail.com>
2020-07-25 23:58:30 +0530

    -- add full CRUD for post

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,4 +16,37 @@ module.exports = {
             console.log(err);
         }
     },
+    async editPost(req, res, next) {
+        try {
+            let post = await Post.findOne({ _id: req.params.id });
+            if (!post) {
+                console.log("NO POST FOUND");
+                return res.redirect("back");
+            }
+            res.render("posts/edit", { post });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async updatePost(req, res, next) {
+        await Post.findByIdAndUpdate(
+            req.params.id,
+            req.body.post,
+            (err, update) => {
+                if (err) {
+                    console.log(err);
+                    return res.redirect("/");
+                }
+                res.redirect("/");
+            }
+        );
+    },
+    async deletePost(req, res, next) {
+        try {
+            await Post.findOneAndRemove({ _id: req.params.id });
+            res.redirect("/");
+        } catch (error) {
+            console.log(error);
+        }
+    },
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { newPost, createPost } = require("../controllers/posts");
+const {
+    newPost,
+    createPost,
+    editPost,
+    deletePost,
+    updatePost,
+} = require("../controllers/posts");
 
 /* GET post new page. */
 router.get("/posts/new", newPost);
 // POST on post page
 router.post("/posts", createPost);
+
+// GET post/:id
+router.get("/posts/:id", editPost);
+// PUT post/:id
+router.put("/posts/:id", updatePost);
+
+// DESTROY POSTS/:id
+router.delete("/posts/:id", deletePost);
 
 module.exports = router;
