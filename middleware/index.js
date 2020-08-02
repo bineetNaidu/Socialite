c967c6791207a178dbb2334c7e1d31d7bc4700ac
bineetNaidu <bineet.s.naidu@gmail.com>
2020-08-02 14:03:30 +0530

    -- fix #issue1 -- proper authorization

--- middleware/index.js
+++ middleware/index.js
@@ -8,47 +8,67 @@ module.exports = {
         res.redirect("/login");
     },
     isCommentsAuthor: async (req, res, next) => {
-        let comment = Comment.findById(req.params.comment_id);
-        if (comment.author.id.equals(req.user.id)) {
-            return next();
+        try {
+            let comment = await Comment.findOne({ _id: req.params.comment_id });
+            if (comment.author.id.equals(req.user._id)) {
+                return next();
+            }
+            req.flash("error", "Your Not Authorized To Do That");
+            res.redirect("back");
+        } catch (error) {
+            req.flash("error", "Oops!, Something Went Wrong!");
+            res.redirect("back");
         }
-        req.flash("error", "Your Not Authorized To Do That");
-        return res.redirect("back");
     },
     isPostsAuthor: async (req, res, next) => {
-        const post = await Post.findById(req.params.id);
-        if (post.author.equals(req.user._id)) {
-            return next();
+        try {
+            const post = await Post.findById(req.params.id);
+            if (post.author.id.equals(req.user._id)) {
+                return next();
+            }
+            req.flash("error", "Your Not Authorized To Do That");
+            res.redirect("back");
+        } catch (error) {
+            req.flash("error", "Oops!, Something Went Wrong!");
+            res.redirect("back");
         }
-        req.flash("error", "Your Not Authorized To Do That");
-        res.redirect("back");
     },
 
     isFraud: async (req, res, next) => {
-        let user = await User.findOne({ _id: req.params.userId });
-        if (user.equals(req.user._id)) {
-            return next();
+        try {
+            let user = await User.findOne({ _id: req.params.userId });
+            if (user.equals(req.user._id)) {
+                return next();
+            }
+            req.flash("error", "Your Not Authorized To Do That");
+            res.redirect("back");
+        } catch (error) {
+            req.flash("error", "Oops!, Something Went Wrong!");
+            res.redirect("back");
         }
-        req.flash("error", "Your Not Authorized To Do That");
-        res.redirect("back");
     },
 
     // validate password
     isValidPassword: async (req, res, next) => {
-        const { user } = await User.authenticate()(
-            req.user.username,
-            req.body.currentPassword
-        );
-        if (user) {
-            // add user to res.locals
-            res.locals.user = user;
-            // go to next middleware
-            next();
-        } else {
-            // flash an error
-            req.flash("error", "Your current password is incorrect!");
-            // short circuit the route middleware and redirect to /profile
-            return res.redirect(`/users/${req.params.userId}/edit`);
+        try {
+            const { user } = await User.authenticate()(
+                req.user.username,
+                req.body.currentPassword
+            );
+            if (user) {
+                // add user to res.locals
+                res.locals.user = user;
+                // go to next middleware
+                next();
+            } else {
+                // flash an error
+                req.flash("error", "Your current password is incorrect!");
+                // short circuit the route middleware and redirect to /profile
+                return res.redirect(`/users/${req.params.userId}/edit`);
+            }
+        } catch (error) {
+            req.flash("error", "Oops!, Something Went Wrong!");
+            res.redirect("back");
         }
     },
     // changing password
